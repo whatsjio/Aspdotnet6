b8ba536a7d969a847764b99b0a4b2e8a5482786c
changsheng fu <909412615@qq.com>
2021-12-06 10:28:38 +0800

    feature:identity过期时间策略

--- AuthServer/Config.cs
+++ AuthServer/Config.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer4;
 using IdentityServer4.Models;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace AuthServer
 {
@@ -43,9 +44,21 @@ namespace AuthServer
                     new Secret("secret".Sha256())//定义客户端秘钥
                 },
                 AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,//授权方式为用户密码模式授权，类型可参考GrantTypes枚举
+                AllowOfflineAccess=true,//需要refresh_tokens 刷新token
+                // AccessTokenLifetime=60, //设置token过期时间60秒 
+                /*Absolute-即RefreshToken的过期策略采用绝对过期时间，即到了AbsoluteRefreshTokenLifetime设置的值后就直接失效
+                Sliding-过期策略采用滑动过期时间，即每次使用这个RefreshToken刷新令牌时，就会重新设置这个RefreshToken的
+                过期时间 = 原本到期时间+滑动过期时间(SlidingRefreshTokenLifetime)但这个时间不会超过
+                AbsoluteRefreshTokenLifetime的值(除非AbsoluteRefreshTokenLifetime的值为0)。*/
+                //RefreshTokenExpiration=TokenExpiration.Absolute,
+                //SlidingRefreshTokenLifetime=60, //设置RefreshToken的滑动过期时间
+                //AbsoluteRefreshTokenLifetime=60, //设置RefreshToken的绝对过期时间
+                // RefreshTokenUsage=TokenUsage.ReUse,//RefreshToken可以反复使用 OneTime-只能使用一次。刷新时会颁发新的RefreshToken
+                // UpdateAccessTokenClaimsOnRefresh  //是否在使用RefreshToken刷新AccessToken的时候，是否返回一个新的AccessToken并更新其声明(Claims)
                 AllowedScopes={
                     "api",
-                    IdentityServerConstants.StandardScopes.OpenId
+                    StandardScopes.OpenId,
+                    StandardScopes.OfflineAccess //如果要获取refresh_tokens ,必须在scopes中加上OfflineAccess
                 }//允许客户端访问的范围
 
             }
